ef8b38ba11e31e067317d7158b5a9df66a2b46eb
Tali <tali.einhorn@k2view.com>
2022-12-05 16:06:39 +0200

    Fix the testers' creation

--- Implementation/LogicalUnits/TDM/Java/resources/TDM/CreateTDMTesterRoleAndUsers/CreateTDMRolesAndUsers.sql
+++ Implementation/LogicalUnits/TDM/Java/resources/TDM/CreateTDMTesterRoleAndUsers/CreateTDMRolesAndUsers.sql
@@ -1,6 +1,8 @@
-create role 'Testers' description "The testers role";
-grant ALL_WS to Testers;
-create USER IF NOT EXISTS 'tester1' WITH PASSWORD '123';
-create USER IF NOT EXISTS 'tester2' WITH PASSWORD '123';
-assign role Testers to user tester1;
-assign role Testers to user tester2;
+create role 'Testersgrp1' description "This is a Tester Role";
+grant ALL_WS ON * to Testersgrp1;
+grant READ ON * to Testersgrp1;
+grant WRITE ON * to Testersgrp1;
+create USER IF NOT EXISTS 'tester1' WITH PASSWORD 't1';
+create USER IF NOT EXISTS 'tester2' WITH PASSWORD 't2';
+assign role Testersgrp1 to user tester1;
+assign role Testersgrp1 to user tester2;
--- Implementation/LogicalUnits/TDM/Java/resources/TDM/CreateTDMTesterRoleAndUsers/CreateTesterPermissionGroup.sql
+++ Implementation/LogicalUnits/TDM/Java/resources/TDM/CreateTDMTesterRoleAndUsers/CreateTesterPermissionGroup.sql
@@ -6,4 +6,4 @@ insert into public.permission_groups_mapping (
 	updated_by,
 	creation_date,
 	update_date
-) values ('Initial mapping for the Testers role', 'Testers', 'tester', 'admin', 'admin', NOW(), NOW());
+) values ('Initial mapping for tester users', 'Testersgrp1', 'tester', 'admin', 'admin', NOW(), NOW());
