8088b06126b635bbd2a2b6e734aae91047423b6d
tdm81-sivan-demo <tdm81-sivan-demo@cloud.k2view.com>
2023-09-18 17:11:33 +0000

    remove admin

--- Implementation/LogicalUnits/TDM/Java/resources/TDM/DEMO/grant_permession.sql
+++ Implementation/LogicalUnits/TDM/Java/resources/TDM/DEMO/grant_permession.sql
@@ -2,7 +2,5 @@ GRANT ALL_WS ON * TO env_owner_role ;
 GRANT BATCH_READ ON * TO env_owner_role ;
 GRANT ALL_WS ON * TO NFT_tester_user ;
 GRANT BATCH_READ ON * TO NFT_tester_user ;
-GRANT ALL_WS ON * TO TDM_admin_role ;   
-GRANT ALL ON * TO TDM_admin_role ;
 GRANT ALL_WS ON * TO UAT_tester_role ;
 GRANT BATCH_READ ON * TO UAT_tester_role ;
