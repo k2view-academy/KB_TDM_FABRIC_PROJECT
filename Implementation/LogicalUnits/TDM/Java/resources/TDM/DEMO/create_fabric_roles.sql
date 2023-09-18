d61845c0b988bf633a4a0cbaae0936a9ba9c1707
tdm81-sivan-demo <tdm81-sivan-demo@cloud.k2view.com>
2023-09-18 16:12:04 +0000

    adding fabric roles

---
+++ Implementation/LogicalUnits/TDM/Java/resources/TDM/DEMO/create_fabric_roles.sql
@@ -0,0 +1,4 @@
+CREATE ROLE 'env_owner_role' description "Role for environment owners";
+CREATE ROLE 'NFT_tester_user' description "Role for testers that perform Non Functional Test (e.g performance test)";
+CREATE ROLE 'TDM_admin_role' description "Role for environment owners";
+CREATE ROLE 'UAT_tester_role' description "Role for testers that perform User Acceptane Tests";
