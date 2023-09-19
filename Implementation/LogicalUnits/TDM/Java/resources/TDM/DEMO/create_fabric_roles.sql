738a50df5a0ff7c9309316f79af466e9acb4a2f0
tdm81-sivan-demo <tdm81-sivan-demo@cloud.k2view.com>
2023-09-19 05:27:02 +0000

    fix descriptions

--- Implementation/LogicalUnits/TDM/Java/resources/TDM/DEMO/create_fabric_roles.sql
+++ Implementation/LogicalUnits/TDM/Java/resources/TDM/DEMO/create_fabric_roles.sql
@@ -1,3 +1,3 @@
 CREATE ROLE 'env_owner_role' description "Role for environment owners";
-CREATE ROLE 'NFT_tester_user' description "Role for testers that perform Non Functional Test (e.g performance test)";
-CREATE ROLE 'UAT_tester_role' description "Role for testers that perform User Acceptane Tests";
+CREATE ROLE 'NFT_tester_user' description "Role for testers performing Non Functional Test (e.g., performance tests)";
+CREATE ROLE 'UAT_tester_role' description "Role for testers performing User Acceptance Tests";
