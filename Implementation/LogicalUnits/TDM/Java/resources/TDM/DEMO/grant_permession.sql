bc43bd3eb85bf20d3e12a838ea714a706605bcfe
implementor1108 <implementor1108@cloud.k2view.com>
2023-11-08 16:08:47 +0000

    changing NFT_tester_user to role

--- Implementation/LogicalUnits/TDM/Java/resources/TDM/DEMO/grant_permession.sql
+++ Implementation/LogicalUnits/TDM/Java/resources/TDM/DEMO/grant_permession.sql
@@ -1,6 +1,6 @@
 GRANT ALL_WS ON * TO env_owner_role ;
 GRANT BATCH_READ ON * TO env_owner_role ;
-GRANT ALL_WS ON * TO NFT_tester_user ;
-GRANT BATCH_READ ON * TO NFT_tester_user ;
+GRANT ALL_WS ON * TO NFT_tester_role ;
+GRANT BATCH_READ ON * TO NFT_tester_role ;
 GRANT ALL_WS ON * TO UAT_tester_role ;
 GRANT BATCH_READ ON * TO UAT_tester_role ;
