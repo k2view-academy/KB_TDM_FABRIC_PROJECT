288dcf463a4c73ab5b0fe0a42aea668704a4ceb7
tahata1 <tahata@k2view.com>
2023-11-14 16:26:09 +0200

    Init count of Reference table

--- Implementation/LogicalUnits/TDM_Reference/Java/src/com/k2view/cdbms/usercode/lu/TDM_Reference/Globals.java
+++ Implementation/LogicalUnits/TDM_Reference/Java/src/com/k2view/cdbms/usercode/lu/TDM_Reference/Globals.java
@@ -9,6 +9,9 @@ import com.k2view.cdbms.shared.utils.UserCodeDescribe.*;
 
 public class Globals extends SharedGlobals {
 
+	@category("TDM")
+	public static String REF_TABLE_COUNT = "NOT_SET";
+
 	
 
 
