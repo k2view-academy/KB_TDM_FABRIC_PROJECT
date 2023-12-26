11bb7abb897cf3935f3349cb07f3b5dcdaa5cabc
siavn-tester-81 <siavn-tester-81@cloud.k2view.com>
2023-12-26 14:33:18 +0000

    last two fixes

--- Implementation/LogicalUnits/TDM/Java/resources/TDM/DEMO/tdm_insert.sql
+++ Implementation/LogicalUnits/TDM/Java/resources/TDM/DEMO/tdm_insert.sql
@@ -42,8 +42,6 @@ INSERT INTO public.products (product_name, product_description,  product_version
 INSERT INTO public.products (product_name, product_description,  product_versions, product_id, product_created_by, product_creation_date, product_last_updated_date, product_last_updated_by, product_status) VALUES ('Collection', 'Collection Application', '1', 3, 'admin', now(), now(), 'admin', 'Active') ON CONFLICT DO NOTHING;
 INSERT INTO public.products (product_name, product_description,  product_versions, product_id, product_created_by, product_creation_date, product_last_updated_date, product_last_updated_by, product_status) VALUES ('Ordering', 'Ordering Application', '1', 4, 'admin', now(), now(), 'admin', 'Active') ON CONFLICT DO NOTHING;
 
-INSERT INTO public.tdm_reserved_entities VALUES ('532', 1, 2, 1, 1, '2023-12-25 12:17:15.070489', '2035-12-25 12:17:15.037', 'admin', NULL, NULL, NULL) ON CONFLICT DO NOTHING;
-
 INSERT INTO public.tdm_be_post_exe_process (process_id, process_name, process_description, be_id, execution_order) VALUES (1, 'postTaskExePrintToLog', 'This is a Post Execution Process Flow.', 1, 1) ON CONFLICT DO NOTHING;
 SELECT pg_catalog.setval('public.business_entities_be_id_seq', 1, true);
 SELECT pg_catalog.setval('public.environment_product_id_seq', 8, true);
