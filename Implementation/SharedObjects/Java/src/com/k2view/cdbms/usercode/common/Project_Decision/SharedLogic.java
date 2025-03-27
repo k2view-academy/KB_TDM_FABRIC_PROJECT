4e8e081c636a84d83c83b726c40b67db32783a09
new-9-3-testers <new-9-3-testers@cloud.k2view.com>
2025-03-27 15:01:28 +0000

    Added SharedGlobals import to SharedLogic in Project_Decision

--- Implementation/SharedObjects/Java/src/com/k2view/cdbms/usercode/common/Project_Decision/SharedLogic.java
+++ Implementation/SharedObjects/Java/src/com/k2view/cdbms/usercode/common/Project_Decision/SharedLogic.java
@@ -24,6 +24,7 @@ import static com.k2view.cdbms.shared.user.ProductFunctions.*;
 import static com.k2view.cdbms.shared.user.UserCode.*;
 import static com.k2view.cdbms.shared.utils.UserCodeDescribe.FunctionType.*;
 import static com.k2view.cdbms.usercode.common.TDM.SharedGlobals.*;
+import static com.k2view.cdbms.usercode.common.SharedGlobals.*;
 
 @SuppressWarnings({"DefaultAnnotationParam"})
 public class SharedLogic {
