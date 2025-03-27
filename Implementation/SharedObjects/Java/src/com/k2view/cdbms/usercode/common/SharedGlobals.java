c99392a8f44bb5b3dea187d3e95db307cdf03447
new-tester-9-3 <new-tester-9-3@cloud.k2view.com>
2025-03-27 14:22:11 +0000

    set PRODUCT_VERSION in Java shared

--- Implementation/SharedObjects/Java/src/com/k2view/cdbms/usercode/common/SharedGlobals.java
+++ Implementation/SharedObjects/Java/src/com/k2view/cdbms/usercode/common/SharedGlobals.java
@@ -7,4 +7,10 @@ package com.k2view.cdbms.usercode.common;
 import com.k2view.cdbms.shared.utils.UserCodeDescribe.*;
 
 public class SharedGlobals {
+
+@category("TDM_DEMO")
+    public static String DEVELOPMENT_PRODUCT_VERSION = "DEV";
+    @category("TDM_DEMO")
+    public static String PRODUCTION_PRODUCT_VERSION = "PROD";
+
 }
