8d9eab33cca24b3fd928109eeb3b05ae620c4c30
daniella.davis.training <daniella.davis.training@k2view.com>
2026-03-23 14:49:03 +0000

    upgrade with Taha

--- Implementation/SharedObjects/Java/src/com/k2view/cdbms/usercode/common/TDM/SharedGlobals.java
+++ Implementation/SharedObjects/Java/src/com/k2view/cdbms/usercode/common/TDM/SharedGlobals.java
@@ -8,88 +8,89 @@ import com.k2view.cdbms.shared.utils.UserCodeDescribe.*;
 
 public class SharedGlobals {
 
-	@desc("Maximum values of combo box input object")
+    @desc("Maximum values of combo box input object")
 	@category("TDM")
-	public static final String COMBO_MAX_COUNT = "100";
+	public static String COMBO_MAX_COUNT = "100";
 
-	@desc("The TTL for masking cache")
+    @desc("The TTL for masking cache")
 	@category("TDM")
-	public static final String MASKING_CACHE_TTL = "2592000";
+	public static String MASKING_CACHE_TTL = "2592000";
 
-	@desc("The maximum number of entities to be returned to be displayed in list of entities")
+    @desc("The maximum number of entities to be returned to be displayed in list of entities")
 	@category("TDM")
 	public static final String MAX_NUMBER_OF_ENTITIES_IN_LIST = "100";
 
-	@desc("Each Instance can have a TTL, this global holds the type of the TTL and it can have one of the following values:Minutes, Hours, Days, Weeks, or Years")
+    @desc("Each Instance can have a TTL, this global holds the type of the TTL and it can have one of the following values:Minutes, Hours, Days, Weeks, or Years")
 	@category("TDM")
 	public static String TDM_LU_RETENTION_PERIOD_TYPE = "Days";
 
-	@desc("The value of the TTL based on the type defined in TDM_LU_RETENTION_PERIOD_TYPE. Populate this global with zero or empty value to avoid setting a TTL on the TDM LUIs.")
+    @desc("The value of the TTL based on the type defined in TDM_LU_RETENTION_PERIOD_TYPE. Populate this global with zero or empty value to avoid setting a TTL on the TDM LUIs.")
 	@category("TDM")
 	public static String TDM_LU_RETENTION_PERIOD_VALUE = "10";
 
-	@desc("The max number of retrieved entities from TDM_RESERVED_ENTITIES, if set to zero then no limit")
+    @desc("The max number of retrieved entities from TDM_RESERVED_ENTITIES, if set to zero then no limit")
 	@category("TDM")
 	public static String GET_RESERVED_ENTITIES_LIMIT = "0";
 
-	@category("TDM")
-	public static final String TDM_REF_UPD_SIZE = "1000";
+    @category("TDM")
+	public static String TDM_REF_UPD_SIZE = "1000";
 
-	@category("TDM")
+    @category("TDM")
 	public static final String TDMDB_SCHEMA = "k2tdm_${ClusterID}";
 
-	@category("TDM")
+    @category("TDM")
 	public static final String TDM_SUMMARY_REPORT_LIMIT = "10000";
 
-	@category("TDM")
+    @category("TDM")
 	public static final String TDM_DELETE_TABLES_PREFIX = "TAR_";
 
-	@category("TDM")
+    @category("TDM")
 	public static final String TDM_BATCH_LIMIT = "-1";
 
-	@category("TDM")
+    @category("TDM")
 	public static final String TDM_SEQ_REPORT = "true";
 
-	@desc("Indicates whether to run add TDM statitics")
+    @desc("Indicates whether to run add TDM statitics")
 	@category("TDM")
 	public static final String TDM_POPULATE_JMX_STATS = "false";
 
-	@desc("The interface to be used for storing the cache of Masking")
+    @desc("The interface to be used for storing the cache of Masking")
 	@category("TDM")
-	public static final String SEQ_CACHE_INTERFACE = "POSTGRESQL_ADMIN";
+	public static String SEQ_CACHE_INTERFACE = "POSTGRESQL_ADMIN";
 
-	@desc("The separator to be used for parsing the values of Parameters")
+    @desc("The separator to be used for parsing the values of Parameters")
 	@category("TDM")
 	public static final String TDM_PARAMETERS_SEPARATOR = "<#>";
 
-	@category("AI")
+    @category("AI")
 	public static final String AI_DB_INTERFACE = "AI_DB";
 
-	@category("AI")
+    @category("AI")
 	public static final String AI_ENVIRONMENT = "AI";
 
-	@category("RULE_BASED")
+    @category("RULE_BASED")
 	public static String SYNTHETIC_ENVIRONMENT = "Synthetic";
 
-	@category("TDM")
+    @category("TDM")
 	public static final String CREATE_AI_K2SYSTEM_DB = "false";
 
-	@category("TDM")
+    @category("TDM")
 	public static String TDM_SUPPRESS_TEST_CONNECTION = "false";
 
-	@category("TDM")
+    @category("TDM")
 	public static final String POP_FULL_LU_HIERARCHY_IN_TDM_LU = "true";
 
-	@category("TDM")
+    @category("TDM")
 	public static String UPDATE_MDB_EXPORTED_SCHEMA = "false";
 
-	@category("TDM")
+    @category("TDM")
 	public static String REPLACE_SEQ_BY_LUI_SYNC = "false";
 
-	@category("TDM")
+    @category("TDM")
 	public static String TDM_USING_CATALOG_SEQUENCES = "false";
 
-	@category("TDM")
+    @category("TDM")
 	public static String CREATE_PHYSICAL_FK_IN_MDB_EXPORT_SCHEMA = "true";
+
 
 }
